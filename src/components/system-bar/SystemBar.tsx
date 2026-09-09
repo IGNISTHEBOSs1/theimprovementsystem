3f03918c9b13e07ac8fd387dafc6d0a6ca065eca
IGNISTHEBOSs1 <arjunoct600@gmail.com>
2026-09-09 18:01:48 +0530

    Update SystemBar.tsx

--- src/components/system-bar/SystemBar.tsx
+++ src/components/system-bar/SystemBar.tsx
@@ -177,7 +177,7 @@ export default function SystemBar({ username }: SystemBarProps) {
           z-40
           flex
           items-stretch
-          h-[60px]
+          h-[56px]
           rounded-full
           border
           border-border/50
